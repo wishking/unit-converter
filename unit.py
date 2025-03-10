cb34db379b04bca45a32bcddce2e7ebb8b6bfd1a
Waqar Ahmed <wwishking@gmail.com>
2025-03-10 13:10:33 +0500

    first commit

---
+++ unit.py
@@ -0,0 +1,49 @@
+import streamlit as st
+
+st.title("🌎 Unit Converter App")
+st.markdown("### Convert Length, Weight, and Time Instantly")
+st.write("Welcome! Select a category, enter a value, and get the conversion result in real time.")
+
+category = st.selectbox("Choose a category", ["Length", "Weight", "Time"])
+
+def convert_units(category, value, unit):
+    if category == "Length":
+        if unit == "Kilometers to Miles":
+            return value * 0.621371
+        elif unit == "Miles to Kilometers":
+            return value / 0.621371
+
+    elif category == "Weight":
+        if unit == "Kilograms to Pounds":
+            return value * 2.20462
+        elif unit == "Pounds to Kilograms":
+            return value / 2.20462
+
+    elif category == "Time":
+        if unit == "Seconds to Minutes":
+            return value / 60
+        elif unit == "Minutes to Seconds":
+            return value * 60
+        elif unit == "Minutes to Hours":
+            return value / 60
+        elif unit == "Hours to Minutes":
+            return value * 60
+        elif unit == "Hours to Day":
+            return value / 24
+        elif unit == "Day to Hours":
+            return value * 24
+
+    return 0
+
+if category == "Length":
+    unit = st.selectbox("📏 Select Conversion", ["Miles to Kilometers", "Kilometers to Miles"])
+elif category == "Weight":
+    unit = st.selectbox("⚖ Select Conversion", ["Kilograms to Pounds", "Pounds to Kilograms"])
+elif category == "Time":
+    unit = st.selectbox("⏳ Select Conversion", ["Seconds to Minutes", "Minutes to Seconds", "Minutes to Hours", "Hours to Minutes", "Hours to Day", "Day to Hours"])
+
+value = st.number_input("Enter the value to convert")
+
+if st.button("Convert"):
+    result = convert_units(category, value, unit)
+    st.success(f"The result is {result:.2f}")
